2be931d74a07a1af25c6836062f850c2f333b092
agadfs <henriquedefreitasserra@hotmail.com>
2024-04-03 16:12:48 -0300

    v2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,30 +3,30 @@ import { useAppContext } from '../AppContext';
 import { useEffect } from 'react';
 
 export default function Navbar() {
-  const { user, setUser } = useAppContext();
+  const { user, setUser, urlrequest } = useAppContext();
   useEffect(() => {
-    if(user.id){
+    if (user.id) {
       localStorage.setItem('userid', user.id)
     }
-    else{
+    else {
       const id = localStorage.getItem('userid')
-      if(id){
-        setUser({id:id})
+      if (id) {
+        setUser({ id: id })
       }
     }
-  },[user])
+  }, [user])
   return (
     <div className={styles.body} >
 
       <div>{user.id ?
         <div>
-          <div> 
-          Seu id: {user.id}
+          <div>
+            Seu id: {user.id}
           </div>
-          <button onClick={()=> {
-             setUser({id:''})
-             localStorage.removeItem('userid')
-             window.location.href='/'
+          <button onClick={() => {
+            setUser({ id: '' })
+            localStorage.removeItem('userid')
+            window.location.href = '/'
           }} >
             Logout
           </button>
@@ -39,12 +39,14 @@ export default function Navbar() {
             Faça o login aqui
           </button>
         </div>}
-      <button onClick={()=> {
-             
-             window.location.href='/'
-          }} >
-            Ir para a pagina inicial
-          </button>
+        <button onClick={() => {
+
+          window.location.href = '/'
+        }} >
+          Ir para a pagina inicial
+        </button>
+
+        {urlrequest === process.env.REACT_APP_API_URL ? <div>Servidor principal ON</div> : <div>SERVIDOR PRINCIPAL ESTÁ OFF (Utilizando servidor backup, talvez tenha algum lag)</div>}
       </div>
     </div>
   )
